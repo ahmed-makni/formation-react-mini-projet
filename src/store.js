4ad24ebfa1051cef069264700fc2975e1f4b7705
ahmed <ahmed.makni.1@esprit.tn>
2021-05-19 11:53:59 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { userListReducer } from "./reducers/UserReducer";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import {rootReducer} from "./reducers/rootReducer";
+import { rootReducer } from "./reducers/rootReducer";
 
 const middleware = [thunk];
 
