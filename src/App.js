30b9deb474ee620ce3516adeeef8466d8b38ba2a
ahmed <ahmed.makni.1@esprit.tn>
2021-05-19 13:55:18 +0100

    reduce

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ import { getUsers } from "./actions/userActions";
 import React from "react";
 
 function App() {
+  const pagination = {
+    page: 1,
+    per_page: 10,
+  };
+
   const dispatch = useDispatch();
   const { userListData } = useSelector(({ users }) => users);
   useEffect(() => {
     if (userListData) {
-      dispatch(getUsers("/users"));
+      dispatch(
+        getUsers(
+          `/users?page=${pagination.page}&per_page=${pagination.per_page}`,
+          pagination
+        )
+      );
     }
+    console.log("userListData : " + userListData);
   }, []);
-  return <div className="App" />;
+  return (
+    <div className="App">
+      {userListData?.data?.map((e, i) => {
+        return <div key={i}>{e.email}</div>;
+      })}
+    </div>
+  );
 }
 
 export default App;
--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,15 +1,18 @@
 import { api } from "../config/constants/axiosParam";
 import { GET_USER_LIST } from "../config/constants/userConstants";
 
-export function getUsers(params) {
-  return async (dispatch) => {
-    return await api.get(params).then(({ data }) => {
-      if (data) {
-        dispatch({
-          type: GET_USER_LIST,
-          payload: data,
-        });
-      }
-    });
-  };
-}
+export const getUsers = (params) => async (dispatch) => {
+  try {
+    const { data } = await api.get(params);
+    if (data) {
+      console.log("users : " + JSON.stringify(data));
+      console.log("users : " + data);
+      return dispatch({
+        type: GET_USER_LIST,
+        payload: data,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
