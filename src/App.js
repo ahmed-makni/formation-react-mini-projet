5f2c9eede4198956946e9dc6a52a47ec70b0d6a5
ahmed <ahmed.makni.1@esprit.tn>
2021-05-19 11:54:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "./actions/userActions";
 import React from "react";
 
-const api = axios.create({
-  baseURL: "https://reqres.in/api",
-});
 function App() {
   const dispatch = useDispatch();
   const { userListData } = useSelector(({ users }) => users);
